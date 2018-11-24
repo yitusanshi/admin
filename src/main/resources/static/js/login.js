e0a0544e570fca3c2c395eef2f1f3f79fe4c2ce8
九九 <875253371@qq.com>
2018-11-24 16:50:43 +0800

    更改登录界面，并提取js代码

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,111 +1,56 @@
-		$(function() {
-			$('input').iCheck({
-				checkboxClass : 'icheckbox_square-blue',
-				radioClass : 'iradio_square-blue',
-				increaseArea : '20%' // optional
-			});
-			
-			
-			// 注册账户
-			$("#regist_btn").click(
-					function(){
-						if(true){
-							type : "POST",
-							url : _ctx + "/regist",
-							data : {
-								username : $("#regist_username").val(),
-								password : $("#regist_password1").val(),
-								firm_name: $("#regist_firm_name").val(),
-								firm_type: $("#regist_firm_type").val(),
-								firm_address1: $("#cmbProvince").val(),
-								firm_address2: $("#cmbCity").val()
-								time : new Date().getTime()
-							},
-							dataType : "json",
-							cache : false,
-							success : function(data) {
-								if ("success" == data.status) {
-									window.location.href = _ctx + "/index";
-								} else {
-									$("#error-msg").html(data.msg);
-								}
+// 注册账户
+$("#regist_btn").click(
+		function() {
+			if (true) {
+				$.ajax({
+					type : "POST",
+					url : _ctx + "/regist",
+					scriptCharset : "utf-8",
+					data : {
+						username : $("#regist_username").val(),
+						password1 : $("#regist_password1").val(),
+						password2 : $("#regist_password2").val(),
+						firm_name : $("#regist_firm_name").val(),
+						productid : $("#productid").val(),
+						address : $("#cmbProvince").val() + ""
+								+ $("#cmbCity").val() + ""
+								+ $("#cmbArea").val() + ""
+								+ $("#regist_address1").val(),
+						founding_time : $("#regist_founding_time").val()
+					},
+					dataType : "json",
+					success : function(data) {
+						if ("success" == data.status) {
+							window.location.href = _ctx + "/index";
+						} else {
+							$("#error-msg1").html(data.msg);
 						}
-					
-			});
-
-			$("#login_btn").click(
-					function() {
-						console.log("$(input[name='username']).val()", $(
-								"input[name='username']").val());
-						console.log("$(input[name='username']).val()", $(
-								"input[name='password']").val());
-						if (checkLogin()) {
-							$.ajax({
-								type : "POST",
-								url : _ctx + "/login",
-								data : {
-									username : $("input[name='username']")
-											.val(),
-									password : $("input[name='password']")
-											.val(),
-									time : new Date().getTime()
-								},
-								dataType : "json",
-								cache : false,
-								success : function(data) {
-									if ("success" == data.status) {
-										window.location.href = _ctx + "/index";
-									} else {
-										$("#error-msg").html(data.msg);
-									}
-								}
-							})
-						}
-						return false;
-					});
-
-			function checkLogin() {
-				if ($("input[name='username']").val() == "") {
-					alert("username cannot be empty")
-					$("input[name='username']").focus();
-					return false;
-				}
-				if ($("input[name='password']").val() == "") {
-					alert("password cannot be empty")
-					$("input[name='password']").focus();
-					return false;
-				}
-				return true;
+					}
+				})
 			}
-			// 打开会员注册
-			$("#Regist_start_").click(function() {
-				$("#login-box").hide();
-				$("#regist-box").show();
-			});
-			$("#toLogin").click(function() {
-				$("#regist-box").hide();
-				$("#login-box").show();
-			});
 		});
 
-function checkLogin() {
-	if ($("input[name='username']").val() == "") {
-		alert("username cannot be empty")
-		$("input[name='username']").focus();
-		return false;
-	}
-	if ($("input[name='password']").val() == "") {
-		alert("password cannot be empty")
-		$("input[name='password']").focus();
-		return false;
-	}
-	return true;
-};
+// 重置内容
+$("#reset_btn").click(function() {
+	$("#regist_username").val("");
+	$("#regist_password1").val("");
+	$("#regist_password2").val("");
+	$("#regist_firm_name").val("");
+	$("#productid").val("1");
+	$("#cmbProvince").val("-省-");
+	$("#cmbCity").val("-市-");
+	$("#cmbArea").val("-区-");
+	$("#regist_address1").val("");
+	$("#regist_founding_time").val("2018-08-08");
 
+});
 
-
-
-
-
-
-
+// 打开会员注册
+$("#Regist_start_").click(function() {
+	$("#login-box").hide();
+	$("#regist-box").show();
+});
+$("#toLogin").click(function() {
+	$("#regist-box").hide();
+	$("#login-box").show();
+});
