1451c885a442797c9e5c547e86b308b719896a71
yitusanshi <843032205@qq.com>
2019-02-23 15:58:56 +0800

    石油换算

--- src/main/java/com/lrs/admin/service/CisRubberService3.java
+++ src/main/java/com/lrs/admin/service/CisRubberService3.java
@@ -24,8 +24,8 @@ public class CisRubberService3 extends BaseJudgeService{
                     break;
                 }
             }
-            float result = divided(productvolume, volume)*0.7;
-            b = true;f
+            float result = divided(productvolume, volume)*0.7f;
+            b = true;
             dataRecordCategory.setProductVolume(result);
             return  transFormToExtend(dataRecordCategory, b, 0f, result);
         }
